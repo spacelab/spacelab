6fca41fb7f4201b805d41d36f877b3fad459854e
Daniel Blomeyer <blomeyer@users.noreply.github.com>
2014-11-14 11:25:26 +0100

    add watcher for patternlab files

---
+++ grunt-tasks/grunt-contrib-watch.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.config('watch', {
+    patternlab: {
+      files: [
+        'patternlab/source/_patterns/**/*.mustache',
+        'patternlab/source/_patterns/**/*.json',
+        'patternlab/source/_data/*.json',
+      ],
+      tasks: [ 'shell:patternlab' ],
+      options: {
+        spawn: false,
+        livereload: true
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
