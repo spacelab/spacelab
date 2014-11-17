25669a8c7ccc3ac139472771337cd806020be837
Daniel Blomeyer <blomeyer@users.noreply.github.com>
2014-11-17 16:20:29 +0100

    remove paternlab entirely

--- grunt-tasks/grunt-contrib-watch.js
+++ grunt-tasks/grunt-contrib-watch.js
@@ -2,18 +2,6 @@ module.exports = function(grunt) {
   'use strict';
 
   grunt.config('watch', {
-    patternlab: {
-      files: [
-        'patternlab/source/_patterns/**/*.mustache',
-        'patternlab/source/_patterns/**/*.json',
-        'patternlab/source/_data/*.json',
-      ],
-      tasks: [ 'shell:patternlab' ],
-      options: {
-        spawn: false,
-        livereload: true
-      }
-    },
     sass: {
       files: [
         'source/**/*.scss'
