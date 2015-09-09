1c52e45a6c3b7d4527e730f26649fab045b777f8
Daniel Blomeyer <blomeyer@users.noreply.github.com>
2015-09-10 00:30:32 +0200

    use uglify for concat or minify

--- grunt-tasks/grunt-contrib-uglify.js
+++ grunt-tasks/grunt-contrib-uglify.js
@@ -2,17 +2,37 @@ module.exports = function (grunt) {
   'use strict';
 
   grunt.config('uglify', {
-    options: {
-      preserveComments: 'some',
-      sourceMap: true
-    },
-    build: {
+    concat: {
+      options: {
+        preserveComments: 'all',
+        sourceMap: true,
+        beautify: true
+      },
       src: [
+        'source/js/libs/**/*.js',
+        'source/js/vendor/**/*.js',
         'source/js/modules/**/*.js',
 
         'source/js/script.js'
       ],
       dest: 'build/js/script.js'
+    },
+    minify: {
+      options: {
+        preserveComments: 'some',
+        sourceMap: false,
+        beautify: false
+      },
+      files: [
+        {
+          expand: true,     // Enable dynamic expansion.
+          cwd: 'build/',      // Src matches are relative to this path.
+          src: ['js/*.js'], // Actual pattern(s) to match.
+          dest: 'build/',   // Destination path prefix.
+          ext: '.js',   // Dest filepaths will have this extension.
+          extDot: 'last'   // Extensions in filenames begin after the first dot
+        },
+      ],
     }
   });
 
